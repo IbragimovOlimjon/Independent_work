ea7d650d7802fd810141e2222a170621c6313097
IbragimovOlimjon <ibragimovolimjon339@gmail.com>
2022-04-18 14:24:09 +0300

    add input disabled

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const elLightModeButton = document.querySelector('.light-link')
-const elDarkModeButton = document.querySelector('.night-link')
+const elLightModeButton = document.querySelector('.light-link');
+const elDarkModeButton = document.querySelector('.night-link');
 
 elLightModeButton.addEventListener('click', function() {
     document.body.classList.add('dark-mode');
@@ -10,4 +10,15 @@ elDarkModeButton.addEventListener('click', function() {
 });
 
 
+const elSelect = document.querySelector ('.main-select');
+const elInput = document.querySelector ('.main-input');
+elSelect.addEventListener('change', function () {
+console.log(elSelect.value);
+if (elSelect.value === 'Other') {
+elInput.disabled = false;
+} else {
+elInput.value = '';
+elInput.disabled = true;
+}
 
+});
